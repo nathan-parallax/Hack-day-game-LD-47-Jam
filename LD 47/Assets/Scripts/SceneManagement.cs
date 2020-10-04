3e8ce09e1e44d46046a95f639bcf0629376af99a
ak3008 <ahmedkhader403@gmail.com>
2020-10-04 16:00:25 +0300

    Separated levels to scenes, added a main menu

---
+++ LD 47/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    private void Start()
+    {
+        PlayerInfo.levelTitle = SceneManager.GetActiveScene().name;
+    }
+
+    public static void LoadRandomLevel()
+    {
+        SceneManager.LoadSceneAsync(Random.Range(1, SceneManager.sceneCountInBuildSettings - 2));
+    }
+
+    public static void QuitApp()
+    {
+        Application.Quit();
+    }
+
+    public static void LoadCredits()
+    {
+        SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings - 1);
+    }
+}
